83ff9813388958b1c273a9ede435ab4577f85b2b
jaime perez <jperez@weblibre.net>
2018-07-15 14:45:58 -0400

    Archivo de configuración del plugin para enlazar una hoja de calculo en google drive con woocommerce

---
+++ wl_gsheetcommerce.php
@@ -0,0 +1,11 @@
+﻿<?php
+/*  
+Plugin Name: WL GSheet Commerce
+Plugin URI: http://www.weblibre.net
+Description:Plugin to connect a Google sheet and import data in sheet to Woocommerce as an external database where you can upload data from a mobile app like appsheet.
+Author: Jaime Pérez
+Version: 0.1
+Author URI: http://www.weblibre.net
+Li9cense: GPL3
+*/
+?>
