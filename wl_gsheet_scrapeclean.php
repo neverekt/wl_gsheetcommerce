281555e01c47519d60765640a4e46dc2be13e4b5
Jaime Perez <jperez@weblibre.net>
2018-09-02 07:53:30 -0300

    Cambios para limpiar y obtener datos desde la hoja de cálculo de google de forma correcta y dar el formato json en utf8 para poder procesar correctamente como json válido.

--- wl_gsheet_scrapeclean.php
+++ wl_gsheet_scrapeclean.php
@@ -1,4 +1,5 @@
 ﻿<?php
+header("Content-Type: application/json");
 $baseUrl = 'https://spreadsheets.google.com/feeds/list/';
 $idGsheet = '1xvmcXGErWGQigpjVkXJrq4kD2lw6hXWrBX6JNSY26v8';
 $endUrl = '/od6/public/values?alt=json';
@@ -6,12 +7,27 @@ $sheetUri = $baseUrl.$idGsheet.$endUrl;
 $file= file_get_contents($sheetUri);
 $json = json_decode($file);
 $rows = $json->{'feed'}->{'entry'};
+$arr = array();
 foreach($rows as $row) {
-  echo '<p>';
-  $code = $row->{'gsx$codigo'}->{'$t'};
-  $prodName = $row->{'gsx$nombreproducto'}->{'$t'};
-  $stock = $row->{'gsx$stock'}->{'$t'};
-  echo "codigo: ". $code . ' / ' . "nombre producto: ". $prodName . ' / ' . "stock: " . $stock;
-  echo '</p>';
+  
+	array_push($arr, array(					
+								'sku' => $row->{'gsx$codigo'}->{'$t'},
+								'name' => $row->{'gsx$nombreproducto'}->{'$t'},
+								'stock_quantity' => $row->{'gsx$stock'}->{'$t'}
+							)
+					);
+
+  // echo "<pre>";
+  // print_r($arr);
+  // echo "</pre>";
+  
+  // echo '<p>';
+  // $code = $row->{'gsx$codigo'}->{'$t'};
+  // $prodName = $row->{'gsx$nombreproducto'}->{'$t'};
+  // $stock = $row->{'gsx$stock'}->{'$t'};
+  // echo "codigo: ". $code . ' / ' . "nombre producto: ". $prodName . ' / ' . "stock: " . $stock;
+  // echo '</p>';
 }
+
+echo json_encode($arr);
 ?>
